31483d31e2d00f79394a04babb47200235c25416
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-10-09 04:58:23 -0500

    rama clase-1

--- index.js
+++
@@ -1,9 +0,0 @@
-const logger = require("./module.js");
-
-logger.info("info function CommonJS");
-logger.log("log function CommonJS");
-
-// para ejecutar index.js aquitar "type":"module" en el .json
-
-const { leerUsuarios } = require("./archivos.js");
-leerUsuarios();
